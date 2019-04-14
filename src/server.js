274a22ed129dca41ea021f878ffdf1560f79721d
João Paulo Gonçalves Sanches <jpfaipa@gmail.com>
2019-04-14 17:07:48 -0300

    Adicionando variaveis ambiente

--- src/server.js
+++ src/server.js
@@ -37,4 +37,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 app.use(require('./routes'))
 
 // Já consegue enxergar tanto requisições HTTP quanto WebSockets
-server.listen(3333)
+server.listen(process.env.PORT || 3333)
